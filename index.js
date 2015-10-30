012b46d675cecb68588870b20869ed47b9dcb67c
greg-nicolle <gnicolle@viadeoteam.com>
2015-10-30 14:06:38 +0100

    [index] Add beer

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ var login_conf = require('./login_conf.json');
 program
   .version(require('./package.json').version)
   .parse(process.argv);
-
  
 // Create simple echo bot 
 login(login_conf, function callback (err, api) {
     if(err) return console.error(err);
  
     api.listen(function callback(err, message) {
-        //api.sendMessage(message.body, message.threadID);
-	console.log(message);
+      if(!err) {
+        if(message.body == 'Bobi?') api.sendMessage(message.senderName+'?', message.threadID);
+        if(message.body == 'Biere?') api.sendMessage('Chaud!', message.threadID);
+      }
     });
 });
