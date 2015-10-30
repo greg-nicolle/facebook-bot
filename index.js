4fc5b8a0b180a19f9ffec6b0fff71f591627dc62
greg-nicolle <gnicolle@viadeoteam.com>
2015-10-30 18:12:25 +0100

    [index] change stuff

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ program
 // Create simple echo bot 
 login(login_conf, function callback (err, api) {
     if(err) return console.error(err);
- 
+
     api.listen(function callback(err, message) {
       if(!err) {
-        if(message.body == 'Bobi?') api.sendMessage(message.senderName+'?', message.threadID);
-        if(message.body.match(/pute/g)) api.sendMessage('C\'est '+message.senderName+' la pute', message.threadID);
-        if(message.body == 'Biere?') api.sendMessage('Chaud!', message.threadID);
+        console.log(message);
+        if(message.body) {
+          if (message.body == 'Bobi?') api.sendMessage(message.senderName + '?', message.threadID);
+          if (message.body.match(/pute/g)) api.sendMessage('C\'est ' + message.senderName + ' la pute', message.threadID);
+          if (message.body == 'Biere?') api.sendMessage('Chaud!', message.threadID);
+          if (message.body.match(/[?]/g)) api.sendMessage(Math.floor((Math.random() * 10))%2 ? 'oui' : 'non', message.threadID);
+        }
       }
     });
 });
