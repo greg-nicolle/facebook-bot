ad3da4ecd692772e4a6fec97a150d61fccf4b4aa
greg-nicolle <gnicolle@viadeoteam.com>
2015-11-26 19:14:36 +0100

    [index] add dico

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ require('./lib.js');
 
 var db = require('./db.js');
 
-var login_conf = require('./login_conf.json');
-
-var insults = require('./insults.json');
+var login_conf = require('./login_conf.json'),
+    insults = require('./insults.json'),
+    insult_response = require('./dico/insult_response.json');
 
 program
     .version(require('./package.json').version)
     .parse(process.argv);
 
-var threadID = 868092476613506;
-
-// Create simple echo bot 
+// Create simple echo bot
 login(login_conf, function callback(err, api) {
   if (err) return console.error(err);
 
@@ -31,20 +29,23 @@ login(login_conf, function callback(err, api) {
           insults.some(ins => message.body.nrml().match(new RegExp(ins, 'g')))? reject(message) : resolve(message)
         })
             .catch(function (msg) {
-              api.sendMessage(msg.senderName + ' c\'est pas bien d\'insulter les gens', msg.threadID);
+              var nb = Math.floor((Math.random() * 100)) % insult_response.length;
+              api.sendMessage(insult_response[nb][0] + msg.senderName + insult_response[nb][1], msg.threadID);
               return Promise.reject();
             })
             .then(function (msg) {
               if (msg.body.match(/[?]/g)) {
                 if (msg.body.nrml().match(/heure/g) && msg.body.length > 10) {
                   api.sendMessage('vers ' + Math.floor((Math.random() * 100)) % 24 + ' heure', msg.threadID);
+                } else if (msg.body.nrml().match(/soir/g) && msg.body.nrml().match(/chaud/g)) {
+                  api.sendMessage('Dans ' + Math.floor((Math.random() * 100)) % 59 + ' minutes', msg.threadID);
                 } else if (msg.body.nrml().match(/quand/g) && msg.body.length > 10) {
                   api.sendMessage('Dans ' + Math.floor((Math.random() * 100)) % 59 + ' minutes', msg.threadID);
                 } else if (msg.body.nrml().match(/qui/g) && msg.body.length > 10) {
                   api.sendMessage('' + msg.participantNames[Math.floor((Math.random() * 100)) % msg.participantNames.length - 1] + '', msg.threadID);
-                } else if (msg.body.nrml().match(/biere/g)){
+                } else if (msg.body.nrml().match(/biere/g)) {
                   api.sendMessage('C\'est mort aujourd\'hui c\'est beaujolais!', msg.threadID);
-                }else if (msg.body.nrml().match(/bobi/g)){
+                } else if (msg.body.nrml().match(/bobi/g)) {
                   api.sendMessage(Math.floor((Math.random() * 10)) % 2 ? 'oui' : 'non', msg.threadID);
                 }
               }
